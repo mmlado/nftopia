e6b8de7682e37a489334eb42f383e71ab30f1188
Mladen Milankovic <developer@mmlado.com>
2023-03-21 21:49:43 +0100

    Add contract owner to constructor and view to ask

--- backend/contracts/NFTopia.vy
+++ backend/contracts/NFTopia.vy
@@ -1,5 +1,14 @@
 # @version ^0.3.0
 
+owner: address
+
 @external
 def __init__():
-  pass
+    assert msg.sender != ZERO_ADDRESS
+    self.owner = msg.sender
+
+@view
+@external
+def isOwner() -> bool:
+    assert msg.sender != ZERO_ADDRESS
+    return self.owner == msg.sender
--- backend/tests/test_nftopia.py
+++ backend/tests/test_nftopia.py
@@ -5,3 +5,6 @@ import pytest
 def nftopia_contract(NFTopia, accounts):
     # deploy the contract with the initial value as a constructor argument
     yield NFTopia.deploy({'from': accounts[0]})
+
+def test_owner(nftopia_contract, accounts):
+    assert nftopia_contract.isOwner()
